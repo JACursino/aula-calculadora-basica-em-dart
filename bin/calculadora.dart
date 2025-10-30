94496ac7e694b4a4be86171d7212d0e4d5b41811
jacursino <jacursino@gmail.com>
2025-10-30 05:46:26 -0300

    adcionando as demais funçoes basicas

--- bin/calculadora.dart
+++ bin/calculadora.dart
@@ -1,16 +1,26 @@
 import 'dart:io';
 
 void main() {
-  print("Vamos fazer a soma entre dois números!");
-
+  print("Vamos fazer uma operação entre dois valores numéricos:");
   //função double faz a conversão de String para número decimal
   double numeroUm = double.parse(stdin.readLineSync()!);
   double numeroDois = double.parse(stdin.readLineSync()!);
 
-  void soma(){
-  print("Nosso resultado é: ${numeroUm + numeroDois}");
+  void soma() {
+    print("Nosso resultado da nossa soma é: ${numeroUm + numeroDois}");
   }
 
-  soma();
-}
+  void subtracao() {
+    print("Nosso resultado da nossa subtração é: ${numeroUm - numeroDois}");
+  }
 
+  void divisao() {
+    print("Nosso resultado da nossa divisão é: ${numeroUm / numeroDois}");
+  }
+
+  void multiplicacao() {
+    print("Nosso resultado da nossa multiplicação é: ${numeroUm * numeroDois}");
+  }
+
+  subtracao();
+}
