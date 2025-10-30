0af6f1adeb2ad067eddc34fa2a9659a1beadd9d1
jacursino <jacursino@gmail.com>
2025-10-30 05:36:12 -0300

    criei a funcao soma

--- bin/calculadora.dart
+++ bin/calculadora.dart
@@ -4,11 +4,13 @@ void main() {
   print("Vamos fazer a soma entre dois números!");
 
   //função double faz a conversão de String para número decimal
-  double numeroUm = double.parse( //doube.parse converte String em double
-    stdin.readLineSync()!,
-  ); /* Converte a entrada do usuário para double; ! indica que a entrada nunca será nula*/
-
+  double numeroUm = double.parse(stdin.readLineSync()!);
   double numeroDois = double.parse(stdin.readLineSync()!);
 
+  void soma(){
   print("Nosso resultado é: ${numeroUm + numeroDois}");
+  }
+
+  soma();
 }
+
