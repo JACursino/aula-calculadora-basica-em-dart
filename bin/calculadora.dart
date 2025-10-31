624813dbc8528daecf3360fc79809355634f134e
jacursino <jacursino@gmail.com>
2025-10-31 06:31:01 -0300

    Otimizacao do bloco if / else

--- bin/calculadora.dart
+++ bin/calculadora.dart
@@ -4,15 +4,20 @@ void main() {
   print('***CALCULADORA BÁSICA EM DART***');
   print("Faça a escolha de dois números e logo em seguida escolha a operação:");
 
-  double numeroUm = double.parse(stdin.readLineSync()!);//função double faz a conversão de String para número decimal
+  double numeroUm = double.parse(
+    stdin.readLineSync()!,
+  ); //função double faz a conversão de String para número decimal
   double numeroDois = double.parse(stdin.readLineSync()!);
-  String operacao = stdin.readLineSync()!; //Tratamento de String -nesse caso sinais de operação
+  String operacao = stdin
+      .readLineSync()!; //Tratamento de String -nesse caso sinais de operação
 
-  void soma() { //Crie a função soma
+  void soma() {
+    //Crie a função soma
     print("O resultado da nossa soma é: ${numeroUm + numeroDois}");
   }
 
-  void subtracao() { // Crie a função subtracao
+  void subtracao() {
+    // Crie a função subtracao
     print("O resultado da nossa subtração é: ${numeroUm - numeroDois}");
   }
 
@@ -24,19 +29,21 @@ void main() {
     print("O resultado da nossa multiplicação é: ${numeroUm * numeroDois}");
   }
 
-  if (operacao == "+") { //Se a operação for igual a "+"
+  if (operacao == "+") {
+    //Se a operação for igual a "+"
     soma(); //Chama a função
+  } else {
+    if (operacao == "-") {
+      subtracao();
+    } else {
+      if (operacao == "*") {
+        multiplicacao();
+      } else {
+        if (operacao == "/") {
+          divisao();
+        }
+      }
+    }
   }
-
-  if (operacao == "-") {
-    subtracao();
-  }
-
-  if (operacao == "*") {
-    multiplicacao();
-  }
-
-  if (operacao == "/") {
-    divisao();
-  }
+  print('***FIM***');
 }
